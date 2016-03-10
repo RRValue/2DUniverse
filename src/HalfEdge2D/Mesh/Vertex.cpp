1dcd606f9869e5e718b42c4f9a7f4ea578f61b9e
Ronti <nasowasaberauch@posteo.de>
2016-03-10 12:12:14 +0100

    add Vertex stub

---
+++ src/HalfEdge2D/Mesh/Vertex.cpp
@@ -0,0 +1,11 @@
+#include "HalfEdge2D/Mesh/Vertex.h"
+
+Vertex::Vertex()
+{
+
+}
+
+Vertex::~Vertex()
+{
+
+}
---
+++ src/HalfEdge2D/Mesh/Vertex.h
@@ -0,0 +1,11 @@
+#ifndef _MESH_VERTEX_H_
+#define _MESH_VERTEX_H_
+
+class Vertex
+{
+public:
+    Vertex();
+    ~Vertex();
+};
+
+#endif //_MESH_VERTEX_H_
