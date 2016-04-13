08aca3a28bc099a1b981697f9005d84e8b8ca202
Ronti <nasowasaberauch@posteo.de>
2016-04-13 17:59:28 +0200

    remove test cases from main

--- src/HalfEdge2D/main.cpp
+++ src/HalfEdge2D/main.cpp
@@ -2,22 +2,8 @@
 
 #include <QtCore/QTimer>
 
-#include "HalfEdge2D/Base/Vector.h"
-#include "HalfEdge2D/Base/StaticPolynomialSolver.h"
-
 int main(int argc, char** argv)
 {
-    StaticPolynomialSolver<float, 1> bla;
-    StaticPolynomialSolver<float, 1>::Result res = bla.solve({1, 2});
-
-    float a = 1.0f;
-    float b = 2.0f;
-
-    std::array<float, 2> aa = {1, 2};
-
-    res = bla.solve(aa);
-    res = bla.solve({&a, &b});
-
     HalfEdge2DApplication app(argc, argv);
 
     QTimer::singleShot(0, &app, &HalfEdge2DApplication::onRun);
